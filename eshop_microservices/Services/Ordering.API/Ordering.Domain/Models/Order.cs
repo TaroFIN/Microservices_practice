a222175609fdc8237492fd85d870c3e67e28b59d
TaroFIN <karta2223899@gmail.com>
2025-04-17 15:21:08 +0800

    Add Ordering.Domain
    Add Ordering.Domain apply with DDD architecture and finished aggregate root

---
+++ eshop_microservices/Services/Ordering.API/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,68 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<OrderId>
+{
+    private readonly List<OrderItem> _orderItems = new();
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; set; } = default!;
+    public OrderStatus Status { get; set; } = default!;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(s => s.Price * s.Quantity);
+        private set { }
+    }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        ArgumentNullException.ThrowIfNull(customerId);
+        ArgumentNullException.ThrowIfNull(orderName);
+        ArgumentNullException.ThrowIfNull(shippingAddress);
+        ArgumentNullException.ThrowIfNull(billingAddress);
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus orderStatus)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = orderStatus;
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+        AddDomainEvent(new OrderItemAddedEvent(this, orderItem));
+    }
+
+    public void Remove(ProductId productId) 
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if(orderItem is null)
+            throw new ArgumentException($"OrderItem with ProductId {productId} not found in order.");
+
+        _orderItems.Remove(orderItem);
+    }
+}
+
---
+++ eshop_microservices/Services/Ordering.API/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    private const int DefaultMaxLength = 50;
+    public string Value { get; }
+    private OrderName(string value) => Value = value;
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultMaxLength);
+        return new OrderName(value);
+    }
+}
+
